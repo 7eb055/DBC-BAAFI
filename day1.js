67a7aafb8e357dfca7f5a5f3b48d6d8d289fa9d8
7eb055 <ryuleboss@gmail.com>
2025-04-25 12:37:37 +0000

    DAY 1 BAAFI

---
+++ day1.js
@@ -0,0 +1,43 @@
+
+let age= 25;
+const name ="Alice";
+console.log(age,name);
+age = 26;
+
+
+//Datatypess
+let isStudent = true;
+let height = 5.9;
+let empty = null;
+let notDefined;
+
+//complex
+let person = {name: "Bob", age: 30};
+
+console.log(person.name);
+console.log(person.age);
+
+
+let fruits = ["boo","appl"];
+console.log(fruits[0]);
+
+
+let x= 10;
+console.log(x + 5);
+console.log(x ==="10");
+console.log(5 > 3 && x < 20);
+
+//operator 
+const a =10, b = 5;
+
+console.log(a - b);
+
+console.log(a / b);
+
+console.log(a % b);
+
+console.log(a ** b);
+
+
+
+console.log(true && false);
---
+++ day1exe.js
@@ -0,0 +1,51 @@
+let price = 100;
+let taxRate = 0.1;
+let quantity = 3;
+
+let totalCost= price * quantity*(1 + taxRate);
+console.log(totalCost);
+//
+quantity = 5;
+totalCost= price * quantity*(1 + taxRate);
+console.log(totalCost);
+
+//
+
+let score =95;
+let message = "Great job!";
+let grades = [90,85,88];
+
+let Student = { name: "Emma", age: 20 };
+
+console.log();
+console.log(score);
+console.log(message);
+console.log(grades);
+console.log(Student);
+
+console.log(grades[1]);
+
+console.log(Student.name);
+
+
+//
+let a=10 , b = 3;
+
+console.log(a + b);
+
+console.log(a % b);
+
+console.log(a**2);
+
+console.log(a > b);
+
+console.log(a===b);
+
+console.log((a>5) && (b<5));
+
+a +=5 ;
+console.log(a);
+
+console.log(5== "5");
+console.log(5=== "5");
+console.log();
